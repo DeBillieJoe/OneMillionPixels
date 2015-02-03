6e159de330ccfa44a4de28882aea57ad71958847
Nightguest <none>
2015-02-03 15:31:12 +0200

    Picture.SaveNew checks for duplicates

--- OneMillionPixels/OneMillionPixels/Database/Picture.cs
+++ OneMillionPixels/OneMillionPixels/Database/Picture.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 
 namespace OneMillionPixels.Database
 {
@@ -25,15 +26,24 @@ namespace OneMillionPixels.Database
             using (DBController dbu = new DBController())
             {
                 var command = dbu.CreateCommand();
-                command.CommandText = @"INSERT INTO Pictures (ID, X, Y, Width, Height, Link, Data) Values(@param1, @param2, @param3, @param4, @param5, @param6, @param7)";
-                command.Parameters.AddWithValue("@param1", ID);
-                command.Parameters.AddWithValue("@param2", X);
-                command.Parameters.AddWithValue("@param3", Y);
-                command.Parameters.AddWithValue("@param4", Width);
-                command.Parameters.AddWithValue("@param5", Height);
-                command.Parameters.AddWithValue("@param6", Link);
-                command.Parameters.AddWithValue("@param7", Data);
-                command.ExecuteNonQuery();
+                command.CommandText = @"INSERT INTO Pictures (ID, X, Y, Width, Height, Link, Data) 
+                                            (SELECT @param1, @param2, @param3, @param4, @param5, @param6, @param7
+                                            WHERE NOT EXISTS
+                                                (SELECT TOP 1 1 FROM Pictures 
+                                                WHERE NOT(X + Width < @param2 OR @param2 + @param4 < X OR Y + Height < @param3 OR @param3 + @param5 < Y)))";
+                command.Parameters.Add("@param1", SqlDbType.NVarChar).Value = ID;
+                command.Parameters.Add("@param2", SqlDbType.Int).Value = X;
+                command.Parameters.Add("@param3", SqlDbType.Int).Value = Y;
+                command.Parameters.Add("@param4", SqlDbType.Int).Value = Width;
+                command.Parameters.Add("@param5", SqlDbType.Int).Value = Height;
+                command.Parameters.Add("@param6", SqlDbType.NText).Value = Link;
+                command.Parameters.Add("@param7", SqlDbType.Image).Value = Data;
+
+
+                int rowsEffected = command.ExecuteNonQuery();
+
+                if (rowsEffected == 0)
+                    throw new Exception("This place is already taken.");
             }
         }
     }
