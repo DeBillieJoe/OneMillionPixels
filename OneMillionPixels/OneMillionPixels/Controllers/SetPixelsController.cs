d8ba89ea026421836887205f91a522f684eb2e99
Ilia Tobov <ilia_tobov@mail.bg>
2015-02-04 14:13:33 +0200

    Rendering images in canvas

--- OneMillionPixels/OneMillionPixels/Controllers/SetPixelsController.cs
+++ OneMillionPixels/OneMillionPixels/Controllers/SetPixelsController.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace OneMillionPixels.Controllers
 {
@@ -13,17 +14,37 @@ namespace OneMillionPixels.Controllers
         public ActionResult Index()
         {
             UploadImageStepOne model = new UploadImageStepOne();
+            model.Images = new List<ImageBanner>();
+            OneMillionPixels.Database.Picture db = new Database.Picture();
+            var images = OneMillionPixels.Database.Picture.RetrieveAll();
+
+            foreach (var image in images)
+            {
+                ImageBanner banner = new ImageBanner();
+                banner.X = image.X;
+                banner.Y = image.Y;
+                banner.Link = image.Link;
+                banner.BinaryContent = image.Data;
+
+                model.Images.Add(banner);
+            }
+
             return View("StepOne", model);
         }
 
-        public ActionResult Upload(HttpPostedFileBase image)
+        public ActionResult Upload(UploadImageStepOne model)
         {
-            OneMillionPixels.Database.Picture db = new Database.Picture() {X=1, Y=1, Width=2, Link="asd" };
-            using (BinaryReader reader = new BinaryReader(image.InputStream))
+            OneMillionPixels.Database.Picture db = new Database.Picture();
+            db.X = model.XCoordinates.Value;
+            db.Y = model.YCoordinates.Value;
+            db.Link = model.Link;
+            db.Width = 100;
+            db.Height = 100;
+            using (BinaryReader reader = new BinaryReader(model.Image.InputStream))
             {
-                db.Data = reader.ReadBytes(image.ContentLength);
+                db.Data = reader.ReadBytes(model.Image.ContentLength);
             }
-
+            db.SaveNew();
             return View("StepTwo"); 
         }
 
--- OneMillionPixels/OneMillionPixels/Models/UploadImageStepOne.cs
+++ OneMillionPixels/OneMillionPixels/Models/UploadImageStepOne.cs
@@ -12,16 +12,26 @@ namespace OneMillionPixels.Models
         [Display(Name="Избор на изображение")]
         public HttpPostedFileBase Image { get; set; }
 
-        //[Required(ErrorMessage = "Моля, Х координата")]
-        //[Display(Name = "Избор на X координата")]
-        //public int? XCoordinates { get; set; } 
+        [Required(ErrorMessage = "Моля, Х координата")]
+        [Display(Name = "Избор на X координата")]
+        public int? XCoordinates { get; set; }
 
-        //[Required(ErrorMessage="Моля, У координата")]
-        //[Display(Name = "Избор на Y координата")]
-        //public int? YCoordinates { get; set; }
+        [Required(ErrorMessage = "Моля, У координата")]
+        [Display(Name = "Избор на Y координата")]
+        public int? YCoordinates { get; set; }
 
-        //[Required(ErrorMessage = "Моля, изберете линк")] 
-        //[Display(Name = "Задайте линк")]
-        //public string Link { get; set; }   
+        [Required(ErrorMessage = "Моля, изберете линк")]
+        [Display(Name = "Задайте линк")]
+        public string Link { get; set; }
+
+        public List<ImageBanner> Images { get; set; }
+    }
+
+    public class ImageBanner
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Link { get; set; }
+        public byte[] BinaryContent { get; set; }
     }
 }
