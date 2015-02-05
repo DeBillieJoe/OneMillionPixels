6969718bc4b6a36da3cf842884b190aac29633ba
Ilia Tobov <ilia_tobov@mail.bg>
2015-02-05 20:38:53 +0200

    commit missing files

---
+++ OneMillionPixels/OneMillionPixels/Models/UploadImageStepTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OneMillionPixels.Models
+{
+    public class UploadImageStepTwo
+    {
+        public UploadImageStepTwo()
+        {
+            Images = new List<ImageBanner>();
+        }
+        [Required(ErrorMessage = "Моля, Х координата")]
+        [Display(Name = "Избор на X координата")]
+        public int? XCoordinates { get; set; }
+
+        [Required(ErrorMessage = "Моля, У координата")]
+        [Display(Name = "Избор на Y координата")]
+        public int? YCoordinates { get; set; }
+
+        [Required(ErrorMessage = "Моля, изберете линк")]
+        [Display(Name = "Задайте линк")]
+        public string Link { get; set; }
+
+        public List<ImageBanner> Images { get; set; }
+    }
+}   
