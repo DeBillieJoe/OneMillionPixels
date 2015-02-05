427a5370880eff5754eaf157d2d73a47a6d2629c
Nightguest <none>
2015-02-05 20:21:24 +0200

    Simplified the database

--- OneMillionPixels/OneMillionPixels/Filters/InitializeSimpleMembershipAttribute.cs
+++ OneMillionPixels/OneMillionPixels/Filters/InitializeSimpleMembershipAttribute.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
+﻿using OneMillionPixels.Models;
+using System;
 using System.Threading;
 using System.Web.Mvc;
 using WebMatrix.WebData;
-using OneMillionPixels.Models;
 
 namespace OneMillionPixels.Filters
 {
@@ -12,13 +10,10 @@ namespace OneMillionPixels.Filters
     public sealed class InitializeSimpleMembershipAttribute : ActionFilterAttribute
     {
         private static SimpleMembershipInitializer _initializer;
-        private static object _initializerLock = new object();
-        private static bool _isInitialized;
 
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            // Ensure ASP.NET Simple Membership is initialized only once per app start
-            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
+            LazyInitializer.EnsureInitialized(ref _initializer);
         }
 
         private class SimpleMembershipInitializer
@@ -29,21 +24,13 @@ namespace OneMillionPixels.Filters
 
                 try
                 {
-                    using (var context = new UsersContext())
-                    {
-                        if (!context.Database.Exists())
-                        {
-                            // Create the SimpleMembership database without Entity Framework migration schema
-                            ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
-                        }
-                    }
                     string connectionString = System.AppDomain.CurrentDomain.BaseDirectory + System.Configuration.ConfigurationManager.AppSettings["DatabasePath"];
                     connectionString = connectionString.Remove(connectionString.LastIndexOf('.'));
-                    WebSecurity.InitializeDatabaseConnection(connectionString, "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection(connectionString, "UserProfile", "UserId", "UserName", autoCreateTables: false);
                 }
                 catch (Exception ex)
                 {
-                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+                    throw new InvalidOperationException("Failed to initialize the database", ex);
                 }
             }
         }
