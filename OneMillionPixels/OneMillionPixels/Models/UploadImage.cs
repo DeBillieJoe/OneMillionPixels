6990973101dcc567fd39f46c4007576f02eca5ec
Ilia Tobov <ilia_tobov@mail.bg>
2015-02-05 10:34:34 +0200

    Not committed changes

---
+++ OneMillionPixels/OneMillionPixels/Models/UploadImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OneMillionPixels.Models
+{
+	public class UploadImage
+	{
+        [Display(Name="Изберете изображение")]
+        [Required]
+        public HttpPostedFileBase Image { get; set; }
+
+        [Display(Name = "Изберете X координата")]
+        [Required]
+        public int XCoordinates { get; set; }
+
+        [Display(Name = "Изберете Y координата")]
+        [Required]
+        public int YCoordinates { get; set; }
+
+        [Display(Name = "Изберете линк")]
+        [Required]
+        public string Link { get; set; } 
+	}
+}
