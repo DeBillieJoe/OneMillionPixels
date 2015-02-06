c4314f3c9a04d1cedd80c2483e72671e9b504d78
Ilia Tobov <ilia_tobov@mail.bg>
2015-02-06 12:30:42 +0200

    home page

--- OneMillionPixels/OneMillionPixels/Controllers/HomeController.cs
+++ OneMillionPixels/OneMillionPixels/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Drawing;
+using Services;
+using OneMillionPixels.Models;
+using Models;
 
 namespace OneMillionPixels.Controllers
 {
@@ -11,50 +14,36 @@ namespace OneMillionPixels.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ImageManager mngr = new ImageManager();
+            var images = mngr.RetrieveAllImages();
+            List<ImageBanner> model = new List<ImageBanner>();
 
-            return View();
+            mapSavedPicturesToImageBanners(images, model);
+
+            return View(model);
         }
 
+        void mapSavedPicturesToImageBanners(List<Picture> images, List<ImageBanner> banners)
+        {
+            foreach (var image in images)
+            {
+                ImageBanner banner = new ImageBanner();
+                banner.ID = image.ID;
+                banner.X = image.X;
+                banner.Y = image.Y;
+                banner.Link = image.Link;
+                banner.BinaryContent = image.Data;
+                banner.ContentType = image.ContentType;
+                banner.Width = image.Width;
+                banner.Height = image.Height;
+
+                banners.Add(banner);
+            }
+        }
         public ActionResult BuyPixels()
         {
-            ViewBag.Message = "Your app description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-
-
-        private Bitmap GetBitmap()
-        {
-            //
-            Bitmap bitmap = new Bitmap(1000, 1000);
-
-            for (int x = 0; x < 1000; x++)
-                for (int y = 0; y < 1000; y++)
-                    bitmap.SetPixel(x, y, Color.DarkRed);
-
-            return bitmap;
-        }
-        public ActionResult GenerateImage()
-        {
-            FileContentResult result;
-            Bitmap bitmap = GetBitmap();
-
-            using (var memStream = new System.IO.MemoryStream())
-            {
-                bitmap.Save(memStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-                result = this.File(memStream.GetBuffer(), "image/jpeg");
-            }
-
-            return result;
-        }
+       
     }
 }
